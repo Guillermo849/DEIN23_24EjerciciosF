dce5946b47fdc00dd4e42a0344be14de544e7bf8
Guillermo849 <orivealonsoguillermo@gmail.com>
2023-10-14 17:47:47 +0200

    git init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ejercicio F
+Ejercicio de DEIN Java FXML que guarda la información que pongamos en una ventana en la tabla de su ventana padre.
+Se podrán modificar y eliminar las personas en la tabla. Para modificar la persona se habrirá una nueva ventana con
+la infomación de está.
+Habrá un TextField donde se podrán filtrar los nombres de las personas en la tabla.
+La tabla se podrá importar o exportar de ficheros csv.
---
+++ src/controllers/TbPersonasController.java
@@ -0,0 +1,150 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+
+import javafx.beans.property.SimpleStringProperty;
+
+import javafx.collections.ObservableList;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import model.Persona;
+
+
+
+import javafx.event.ActionEvent;
+
+public class TbPersonasController implements Initializable{
+
+    @FXML
+    private Button btnAgregarPersona;
+    
+    @FXML
+    private Button btnModificar;
+
+    @FXML
+    private Button btnEliminar;
+
+    @FXML
+    private TableView<Persona> tbViewPersonas;
+
+    @FXML
+    private TableColumn<Persona, String> tbColNombre;
+
+    @FXML
+    private TableColumn<Persona, String> tbColApellidos;
+
+    @FXML
+    private TableColumn<Persona, Integer> tbColEdad;
+    
+    private NuevaPersonaController newPersonaWindow;
+    
+    private static Image ICONO = new Image(Main.class.getResourceAsStream("/img/agenda.png"));
+    
+    private int personaIndex = -1;
+    
+    @FXML
+    void selectPersona(MouseEvent event) {
+    	if (tbViewPersonas.getSelectionModel().getSelectedItem() != null) {
+    		personaIndex = tbViewPersonas.getSelectionModel().getSelectedIndex();
+    	}
+    }
+    
+    @FXML
+    void aniadirPersona(ActionEvent event) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/DatosPersonasAgregar.fxml"));
+			Parent root = loader.load();
+			/* Le dice a la nueva ventana cual es su ventana padre */
+			newPersonaWindow = loader.getController();
+			newPersonaWindow.setParent(this, null);
+			
+			Stage agregarStage = new Stage();
+			agregarStage.setScene(new Scene(root));
+			agregarStage.setResizable(false);
+			agregarStage.getIcons().add(ICONO);
+			agregarStage.setTitle("Nueva Persona");
+			agregarStage.showAndWait();
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+    }
+    
+    @FXML
+    void modificarPersona(ActionEvent event) {
+    	if (personaIndex > -1) {
+	    	try {
+	    		
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/DatosPersonasAgregar.fxml"));
+				Parent root = loader.load();
+				newPersonaWindow = loader.getController();
+				newPersonaWindow.setParent(this, tbViewPersonas.getItems().get(personaIndex));
+				
+				Stage agregarStage = new Stage();
+				agregarStage.setScene(new Scene(root));
+				agregarStage.getIcons().add(ICONO);
+				agregarStage.setTitle("Modificar Persona");
+				agregarStage.showAndWait();
+				
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+    	}
+    }
+    
+    /* Elimina la persona seleccionada*/
+    @FXML
+    void eliminarPersona(ActionEvent event) {
+    	if (personaIndex > -1) {
+    		tbViewPersonas.getItems().remove(personaIndex);
+    	}
+    }
+
+    /*
+     * Añade la informacion de la ventana DatosPersonasAgregarController a la tabla
+     * */
+    public void devolverPersonaNueva(Persona person) {
+    	ObservableList<Persona> obLstPersonas = tbViewPersonas.getItems();
+    	obLstPersonas.add(person);
+        tbViewPersonas.setItems(obLstPersonas);
+    }
+    
+    /*
+     * Añadirá la persona modificada a la tabla
+     * */
+    public void devolverPersonaMod(Persona person) {
+    	tbViewPersonas.getItems().set(personaIndex, person);
+    }
+    
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		tbColNombre.setCellValueFactory(new PropertyValueFactory<Persona, String>("nombre"));
+        
+		tbColApellidos.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getApellido()));
+        
+		tbColEdad.setCellValueFactory(new PropertyValueFactory<Persona, Integer>("edad"));
+	}
+
+}
+
